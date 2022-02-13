65ba8d527830c65213c9f490bfaea3b24bc3505a
carlosrobertoprogramador <carlosrslopes@gmail.com>
2022-02-12 21:58:54 -0300

    create api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,134 @@
+import { Comic } from '../classes/comic';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Md5 } from 'ts-md5/dist/md5';
+import { Character } from '../classes/character';
+import { Serie } from '../classes/series';
+import { Creator } from '../classes/creator';
+import { Storie } from '../classes/storie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComicService {
+  private md5 = new Md5();
+  private publicKey = "6187c0cda296b012c9069d564b82de9d"
+  private privateKey = "8bcbd1a4c2a05485d19ea6e13cba1619b5e50989"
+  private time = new Date().toDateString()
+  private hash = this.md5.appendStr(this.time + this.privateKey + this.publicKey).end()
+  private urlMarvel = "http://gateway.marvel.com/v1/public/"
+  private paramsMarvel = `ts=${this.time}&apikey=${this.publicKey}&hash=${this.hash}`;
+
+  private urlLocal = "http://localhost:3000/"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getComics(): Promise<Comic[]> {
+    return this.httpClient.get<Comic[]>(`${this.urlMarvel}comics?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(comics => {
+              return comics
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  }
+
+  getCharacters(): Promise<Character[]> {
+    return this.httpClient.get<Character[]>(`${this.urlMarvel}characters?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(characters => {
+              return characters
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  };
+
+  getSeries(): Promise<Serie[]> {
+    return this.httpClient.get<Serie[]>(`${this.urlMarvel}series?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(series => {
+              return series
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  };
+
+  getCreators(): Promise<Creator[]> {
+    return this.httpClient.get<Creator[]>(`${this.urlMarvel}creators?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(creators => {
+              return creators
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  };
+
+  getStories(): Promise<Storie[]> {
+    return this.httpClient.get<Storie[]>(`${this.urlMarvel}stories?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(stories => {
+              return stories
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  };
+
+  getEvents(): Promise<Event[]> {
+    return this.httpClient.get<Event[]>(`${this.urlMarvel}events?${this.paramsMarvel}`).toPromise().then(
+      result => {
+        if (result['code'] == 200) {
+          if (result['data']['count'] > 0) {
+            return result['data']['results'].map(events => {
+              return events
+            })
+          } else {
+            return null;
+          }
+        } else {
+          throw new Error('Ocorreu um erro interno de código!');
+        }
+      }
+    );
+  };
+
+}
