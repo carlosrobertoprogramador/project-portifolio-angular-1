0e8bd0a080f579329297956dff5c0d137685c186
carlosrobertoprogramador <carlosrslopes@gmail.com>
2022-02-12 21:59:49 -0300

    create component show cards

---
+++ src/app/components/comics/list/show-cards/show-cards.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Comic } from './../../../../classes/comic';
+
+@Component({
+  selector: 'app-show-cards',
+  templateUrl: './show-cards.component.html',
+  styleUrls: ['./show-cards.component.scss']
+})
+export class ShowCardsComponent implements OnInit {
+  @Input() comics: Comic[] = [];
+  @Input() name: string = '';
+
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  getCreatorsName(creators) {
+    return creators.map(creator => {
+      return creator.name
+    }).join(', ')
+  }
+
+  navigateDetail(id) {
+    this.route.navigate([`/comic/detail/${id}`])
+  }
+
+  navigateCheckout(id) {
+    this.route.navigate([`/comic/checkout/${id}`])
+  }
+
+}
