3270a2f47d5db600994b2ff762b20a7ca70a6bd4
carlosrobertoprogramador <carlosrslopes@gmail.com>
2022-02-12 22:00:14 -0300

    create component list

---
+++ src/app/components/comics/list/list.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit} from '@angular/core';
+import { ComicService } from '../../../services/api.service';
+import { Comic } from './../../../classes/comic';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-comics-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+
+export class ListComponent implements OnInit {
+  public comicsRare: Comic[] = [];
+  public comicsCommon: Comic[] = [];
+  public sliderImages: string[] = [];
+
+  constructor(private comicService: ComicService, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.comicService.getComics().then((comics => {
+      this.getQuantityComicsRare(comics)
+      const quantityComicsRare = this.getQuantityComicsRare(comics);
+      if (quantityComicsRare > 0) {
+        const numbersRandom = this.getNumbersRandom(1, comics.length, quantityComicsRare);
+        comics.map((comic, index) => {
+          index++;
+          if (numbersRandom.includes(index)) {
+            this.comicsRare.push(comic);
+            const image = comic.images[0];
+            if (image) {
+              const path = image.path + '.' + image.extension;
+              this.sliderImages.push(path)
+            }
+          } else {
+            this.comicsCommon.push(comic);
+          }
+        })
+      }
+
+    })).catch((error => {
+      console.error(error);
+      if (error.message) {
+        this.snackBar.open(error.message)
+      } else {
+        this.snackBar.open('Erro interno, contate o suporte por favor!')
+      }
+    }));
+  }
+
+  getQuantityComicsRare(data: Comic[] = []): number {
+    if (data.length > 0) {
+      return this.getNumberRandom(1, data.length);
+    }
+    return 0
+  }
+
+  getNumbersRandom(min, max, quantity): number[] {
+    const numbers = [];
+    for (let index = 0; index < quantity; index++) {
+      const number = this.getNumberRandom(min, max)
+      if (numbers.includes(number)) {
+        index--;
+      } else {
+        numbers.push(this.getNumberRandom(min, max));
+      }
+    }
+    return numbers;
+  }
+
+  getNumberRandom(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+}
