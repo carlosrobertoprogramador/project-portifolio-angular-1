404adb15577f7cccbe81d0dad22227ed9ae22095
carlosrobertoprogramador <carlosrslopes@gmail.com>
2022-02-12 22:01:11 -0300

    alter app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/comics/list/list.component';
+import { DetailComponent } from './components/comics/detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'comic/list', component: ListComponent },
+  { path: 'comic/detail/:id', component: DetailComponent },
+  { path: 'comic/checkout/:id', component: DetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
